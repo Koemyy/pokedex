62286a3d7723ec084b1a029fa621c439a0f47df2
Koemy <francine.pns@gmail.com>
2022-11-24 17:16:19 -0300

    Update App with new component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import TopBar from "./components/TopBar";
 import './App.css'
 import {Sections} from './components/Infos';
+import PokeList from "./components/PokeList";
 
 function App() {
     return (
         <div>
             <TopBar title="Pokédex"/>
+            <PokeList title="aaaa" detail="aaaa"/>
             <div className="fixed bottom-0 lg:h-[50px] w-full bg-black items-center py-3 lg:pr-12">
                 <Sections/>
             </div>
