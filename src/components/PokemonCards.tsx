8538fe9804aa011cec3f712e5ccb1f7b750a7fd2
Koemy <francine.pns@gmail.com>
2022-11-27 12:05:27 -0300

    Add some style

--- src/components/PokemonCards.tsx
+++ src/components/PokemonCards.tsx
@@ -25,13 +25,13 @@ function PokemonCard({id, image, name, type, weight, height, stats}: Card) {
     }
 
     return (
-        <div className="container">
+        <div className="flex lg:justify-end lg:overflow-y-hidden">
             {isShown && (
                 <div className="show">
                     <div className="stat-container-title">
-                        <img className="image-title" src={image} alt={name}/>
+                        <img className="w-20" src={image} alt={name}/>
                         <p className="w-180px text-black">No. {id}</p>
-                        <p>{name}</p>
+                        <p className="pl-10 first-letter:uppercase">{name}</p>
                         <img className="pokeball-title" src={pokeball} alt="A pokeball icon"/>
                     </div>
                     <img className="w-full" src={image} alt={name}/>
@@ -41,8 +41,8 @@ function PokemonCard({id, image, name, type, weight, height, stats}: Card) {
                             <p>Height</p>
                             <p>Weight</p>
                         </div>
-                        <div className="stats-right bg-white">
-                            <p>{type}</p>
+                        <div className="stats-right bg-white text-center">
+                            <p className="first-letter:uppercase">{type}</p>
                             <p>{height} cm</p>
                             <p>{weight} lbs</p>
                         </div>
@@ -51,42 +51,44 @@ function PokemonCard({id, image, name, type, weight, height, stats}: Card) {
                         <div>
                             {
                                 // @ts-ignore
-                                stats.map(stats => <p>
-                                    {stats.base_stat}
+                                stats.map((stats, index) => <p key={index}  className="first-letter:uppercase">
+                                    {stats.stat.name}
                                 </p>)
                             }
                         </div>
                         <div>
                             {
                                 // @ts-ignore
-                                stats.map(stats => <p>
-                                    {stats.stat.name}
+                                stats.map((stats, index) => <p key={index}>
+                                    {stats.base_stat}
                                 </p>)
                             }
                         </div>
                     </div>
                 </div>
             )}
-            <div className="right"
+            <div className="hover:bg-gradient-to-bl from-orange-300 hover:even:text-white to-orange-500 rounded-3xl flex items-center w-[38%] h-[55px] mt-5 mr-4 p-10 text-base font-bold"
                 onMouseEnter={() => setIsShown(true)}
                 onMouseLeave={() => setIsShown(false)}
                 onClick={modalHandler}
             >
-                <img className="max-h-[50px] mr-[10px] w-[50px]" src={image} alt={name}/>
+                <img className="" src={image} alt={name}/>
                 <p className="w-[270px]">No. {id}</p>
-                <p>{name}</p>
+                <p className="first-letter:uppercase">{name}</p>
                 <img className="ml-auto w-[40px]" src={pokeball} alt="A pokeball icon"/>
             </div>
-            {setModalIsOpen && (
-                <Modal>
+            {modalIsOpen && (
+                <Modal
+                    key={id}
                     id={id}
                     name={name}
+                    image={image}
                     height={height}
                     weight={weight}
                     stats={stats}
                     type={type}
                     onClick={closeModalHandler}
-                </Modal>
+                />
             )}
         </div>
     )
