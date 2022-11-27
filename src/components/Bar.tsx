290a98a2c2ba1a2ec5d72b03f7fb7472a8795a02
Koemy <francine.pns@gmail.com>
2022-11-27 15:31:41 -0300

    Remove unused things from footer

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,13 +1,7 @@
-interface Bar {
-    title: string,
-    detail: string
-}
-
-function Bar({title, detail}: Bar) {
+function Bar() {
     return (
-        <div className="inline-flex text-white cursor-pointer justify-center items-center lg:pl-5 px-1">
-            <p className="rounded-lg padding-bar bg-white text-black">{title}</p>
-            <p className="lg:px-1">{detail}</p>
+        <div className="justify-center items-center text-center text-black">
+            <p className="font-bold">Com  ❤️ por Francine Nogueira</p>
         </div>
     )
 }
