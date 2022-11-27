c7f2335a99810f85d6a54c09f3fda297909cc984
Koemy <francine.pns@gmail.com>
2022-11-27 15:32:38 -0300

    Remove ugly things from top-bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,31 +1,9 @@
-import pokeball from '../images/pokeball.png';
+import pokedex from '../images/pokedex_logo.png';
 
-interface topBar {
-    title: string,
-}
-
-function TopBar({title}: topBar) {
+function TopBar() {
     return (
-        <div className="flex items-center justify-between absolute padding top-8 h-12 w-full top-bar-gradient">
-            <div className="flex justify-between items-center lg:w-[40%]">
-                <h1 className="font-lg font-bold">{title}</h1>
-                <div className="flex items-center align-middle text-white">
-                    <div className="flex padding-pokeball items-center align-middle justify-center rounded-lg ml-5 bg-gray-500">
-                        <img src={pokeball}
-                             alt="A pokeball icon"
-                             style={{width: "30px", marginRight: "10px"}}
-                        />
-                        <p>438</p>
-                    </div>
-                    <div className="flex padding-pokeball items-center align-middle justify-center rounded-lg ml-5 bg-gray-500">
-                        <img src={pokeball} alt="A pokeball icon"
-                             style={{width: "30px", marginRight: "10px"}}
-                        />
-                        <p>649</p>
-                    </div>
-                </div>
-            </div>
-            <p className="text-white flex absolute right-0 lg:pr-[350px]">A -&gt; Z</p>
+        <div className="flex items-center absolute w-full lg:py-5 lg:pl-10">
+            <img className="lg:w-[250px] w-48 pt-3 mx-auto lg:mx-0" src={pokedex} alt=""/>
         </div>
     )
 }
