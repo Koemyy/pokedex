8df28a4c3f96aed92ddf29e144475099eb75c008
koemy <francine.pns@gmail.com>
2024-10-03 16:28:56 -0300

    Add loading message when waiting API response

--- src/components/PokeList.tsx
+++ src/components/PokeList.tsx
@@ -1,41 +1,55 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import PokemonCard from './PokemonCards';
 import axios from 'axios';
 
 function PokeList() {
     const [allPokemons, setAllPokemons] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true); 
+
     const getAllPokemons = async () => {
+        setLoading(true); 
         var endpoints = [];
-        for (var i = 1; i < 659; i++) {
+        for (var i = 1; i < 386; i++) {
             endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
         }
-        axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setAllPokemons(res));
+        axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+            .then((res) => {
+                setAllPokemons(res);
+                setLoading(false); 
+            });
     }
 
     useEffect(() => {
-        getAllPokemons()
-    }, [])
+        getAllPokemons();
+    }, []);
+
     return (
         <div className="w-full h-[100vh] bg-pokemon">
             <div className="h-[86vh] absolute top-[80px] lg:right-[30px] overflow-y-scroll w-full">
-                {allPokemons.map((pokemonStats, index) => {
-                    return (
-                        <PokemonCard
-                            key={index}
-                            // @ts-ignore
-                            id={pokemonStats.data.id}
-                            image={pokemonStats.data.sprites.front_default}
-                            name={pokemonStats.data.name}
-                            type={pokemonStats.data.types[0].type.name}
-                            weight={pokemonStats.data.weight}
-                            height={pokemonStats.data.height}
-                            stats={pokemonStats.data.stats}
-                        />
-                    )
-                })}
+                {loading ? (
+                    <div className="flex justify-center items-center h-full">
+                        <p className="text-white text-xl">Loading Pokémons...</p>
+                    </div>
+                ) : (
+                    allPokemons.map((pokemonStats, index) => {
+                        return (
+                            <PokemonCard
+                                key={index}
+                                // @ts-ignore
+                                id={pokemonStats.data.id}
+                                image={pokemonStats.data.sprites.front_default}
+                                name={pokemonStats.data.name}
+                                type={pokemonStats.data.types[0].type.name}
+                                weight={pokemonStats.data.weight}
+                                height={pokemonStats.data.height}
+                                stats={pokemonStats.data.stats}
+                            />
+                        )
+                    })
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default PokeList
+export default PokeList;
